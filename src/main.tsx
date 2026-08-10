e12d6feb1e05efc20911ab21737fc6489bbb349f
Petturick <60271566+Petturick@users.noreply.github.com>
2026-08-10 15:16:45 +0200

    Laad definitieve moodboard visuals

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import "./styles/overrides.css";
 import "./styles/redesign.css";
 import "./styles/brandbook-v3.css";
 import "./styles/moodboard-final.css";
+import "./styles/moodboard-assets.css";
 
 const isAdminRoute = window.location.pathname === "/admin" || window.location.pathname.startsWith("/admin/");
 
