2954f0a2cc294d978a1acf06c532eb0d539560c8
Petturick <60271566+Petturick@users.noreply.github.com>
2026-08-10 11:50:23 +0200

    Route admin path to CMS editor

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AdminApp from "./admin/AdminApp";
 import "./styles/global.css";
 import "./styles/overrides.css";
 import "./styles/redesign.css";
 
+const isAdminRoute = window.location.pathname === "/admin" || window.location.pathname.startsWith("/admin/");
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {isAdminRoute ? <AdminApp /> : <App />}
   </React.StrictMode>
 );
